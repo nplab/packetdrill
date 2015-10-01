afd4a5641ac60bc5fdee938732f6bd3f6987a453
Michael Tüxen <tuexen@fh-muenster.de>
2015-10-01 12:12:39 +0200

    Improve signal handler.

--- gtests/net/packetdrill/run.c
+++ gtests/net/packetdrill/run.c
@@ -66,13 +66,6 @@ const int MAX_SPIN_USECS = 20;
 
 static struct state *state = NULL;
 
-void interrupt_handler(int signal_number) {
-	if (state != NULL)
-		close_all_sockets(state);
-	
-	die("interrupted");
-}
-
 struct state *state_new(struct config *config,
 			struct script *script,
 			struct netdev *netdev)
@@ -502,14 +495,21 @@ static s64 schedule_start_time_usecs(void)
 #endif
 }
 
+void signal_handler(int signal_number) {
+	if (state != NULL)
+		close_all_sockets(state);
+	
+	die("Handled signal %d", signal_number);
+}
+
 void run_script(struct config *config, struct script *script)
 {
 	char *error = NULL;
 	struct netdev *netdev = NULL;
 	struct event *event = NULL;
 	
-	if (signal(SIGINT, interrupt_handler) == SIG_ERR) {
-		die("could not set up interrupt handler for SIGINT!");
+	if (signal(SIGINT, signal_handler) == SIG_ERR) {
+		die("could not set up signal handler for SIGINT!");
 	}
 
 	DEBUGP("run_script: running script\n");
