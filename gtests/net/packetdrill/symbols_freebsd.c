28602f6c961d75b4c889cd9808036fe9626c7c88
hoelscher <jens.hoelscher@fh-muenster.de>
2015-11-27 19:42:22 +0100

    add symbol for socketopt SCTP_PARTIAL_DELIVERY_POINT,

--- gtests/net/packetdrill/symbols_freebsd.c
+++ gtests/net/packetdrill/symbols_freebsd.c
@@ -94,6 +94,7 @@ struct int_symbol platform_symbols_table[] = {
 	{ SCTP_HMAC_IDENT,                  "SCTP_HMAC_IDENT"                 },
 	{ SCTP_AUTH_ACTIVE_KEY,             "SCTP_AUTH_ACTIVE_KEY"            },
 	{ SCTP_DELAYED_SACK,                "SCTP_DELAYED_SACK"               },
+	{ SCTP_PARTIAL_DELIVERY_POINT,      "SCTP_PARTIAL_DELIVERY_POINT"     },
 	{ SCTP_MAX_BURST,                   "SCTP_MAX_BURST"                  },
 	{ SCTP_PEER_ADDR_PARAMS,            "SCTP_PEER_ADDR_PARAMS"           },
 	{ SCTP_EVENT,                       "SCTP_EVENT"                      },
