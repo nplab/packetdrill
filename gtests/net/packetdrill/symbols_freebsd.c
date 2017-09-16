6af840c0d0585e0f4fc53551c69340bd2187b570
Michael Tuexen <tuexen@fh-muenster.de>
2017-09-16 12:05:23 +0200

    This is taken from the FreeBSD ports tree.

--- gtests/net/packetdrill/symbols_freebsd.c
+++ gtests/net/packetdrill/symbols_freebsd.c
@@ -76,7 +76,9 @@ struct int_symbol platform_symbols_table[] = {
 	{ SO_LISTENQLEN,                    "SO_LISTENQLEN"                   },
 	{ SO_LISTENINCQLEN,                 "SO_LISTENINCQLEN"                },
 	{ SO_SETFIB,                        "SO_SETFIB"                       },
+#ifdef SO_USER_COOKIE
 	{ SO_USER_COOKIE,                   "SO_USER_COOKIE"                  },
+#endif
 
 	/* /usr/include/netinet/in.h */
 	{ IP_OPTIONS,                       "IP_OPTIONS"                      },
@@ -607,7 +609,9 @@ struct int_symbol platform_symbols_table[] = {
 	{ ENOLINK,                          "ENOLINK"                         },
 	{ EPROTO,                           "EPROTO"                          },
 	{ ENOTCAPABLE,                      "ENOTCAPABLE"                     },
+#ifdef ECAPMODE
 	{ ECAPMODE,                         "ECAPMODE"                        },
+#endif
 
 	/* Sentinel marking the end of the table. */
 	{ 0, NULL },
