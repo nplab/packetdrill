4fbb586c88a0fa076f95b454164f34448c42a490
Michael Tuexen <tuexen@fh-muenster.de>
2023-03-15 15:35:43 +0100

    Always use the cookie from the latest received INIT ACK

--- gtests/net/packetdrill/run_packet.c
+++ gtests/net/packetdrill/run_packet.c
@@ -3206,6 +3206,7 @@ static int do_outbound_script_packet(
 	}
 
 	if ((socket->state == SOCKET_PASSIVE_PACKET_RECEIVED) ||
+	    (socket->state == SOCKET_PASSIVE_INIT_ACK_SENT) ||
 	    (socket->state == SOCKET_PASSIVE_COOKIE_ECHO_RECEIVED)) {
 		if (packet->tcp && packet->tcp->syn && packet->tcp->ack && !(packet->flags & FLAG_IGNORE_SEQ)) {
 			/* Script says we should see an outbound server SYNACK. */
@@ -3257,6 +3258,7 @@ static int do_outbound_script_packet(
 				goto out;
 			}
 			if (((socket->state == SOCKET_PASSIVE_PACKET_RECEIVED) ||
+			     (socket->state == SOCKET_PASSIVE_INIT_ACK_SENT) ||
 			     (socket->state == SOCKET_PASSIVE_COOKIE_ECHO_RECEIVED)) &&
 			    (chunk->type == SCTP_INIT_ACK_CHUNK_TYPE)) {
 				chunk_length = ntohs(chunk->length);
