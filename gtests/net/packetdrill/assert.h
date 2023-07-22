80d10b3b2aad3138e5345ec407748957c8a08d64
Michael Tuexen <tuexen@fh-muenster.de>
2023-07-21 22:04:41 -0700

    Add newline at the end of the error message

--- gtests/net/packetdrill/assert.h
+++ gtests/net/packetdrill/assert.h
@@ -5,6 +5,6 @@ extern void __attribute__((noreturn)) die(char *format, ...);
 #define assert(expr)						\
 	do {							\
 		if (!(expr))					\
-			die("assertion %s failed at %s line %d",\
+			die("assertion %s failed at %s line %d\n",\
 			    #expr, __FILE__, __LINE__);\
 	} while (0)
